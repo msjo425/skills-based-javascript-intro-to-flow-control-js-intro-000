570dcc91feae40b48cc09884c2565a01ab24e778
matt joseloff <msjo425@gmail.com>
2020-04-10 20:43:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,5 @@
 function basicTeenager(age) {
-
+if (age>12 && age<20){
+  return "You are a teenager!"
 }
-
-function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
 }
